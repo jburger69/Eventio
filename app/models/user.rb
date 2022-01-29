02ec92a98f158c5cc0bce737cfb95bd7bc61e210
Joshua Burger <joshberger6969@outlook.com>
2022-01-29 15:25:03 -0500

    Has many associations for event_attendences

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event', dependent: :destroy
+  has_many :event_attendences, foreign_key: :event_attendence_id
+  has_many :attended_events, through: :event_attendences
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
