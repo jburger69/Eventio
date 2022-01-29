7d9312e56f893668b8615ef113fa37d127c6d573
Joshua Burger <joshberger6969@outlook.com>
2022-01-29 15:24:45 -0500

    Create has_many relation through event_attendences

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ApplicationRecord
     
     belongs_to :creator, class_name: 'User'
+    has_many :event_attendences, foreign_key: :attended_event_id
+    has_many :event_attendees, through: :event_attendences, source: :event_attendence
+    scope :past_events, -> { where('date < ?', Date.today) }
+    scope :future_events, -> { where('date >= ?', Date.today) }
 
 end
