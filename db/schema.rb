5356925e432977f6146a80cb1b2114c6549af598
Joshua Burger <joshberger6969@outlook.com>
2022-01-29 15:27:09 -0500

    Create new event_attendences table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_042948) do
+ActiveRecord::Schema.define(version: 2022_01_29_191353) do
+
+  create_table "event_attendences", force: :cascade do |t|
+    t.integer "event_attendence_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "date"
