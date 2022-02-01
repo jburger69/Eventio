e435b12119339a1e8f937a11c79f96f1e81b66ba
Joshua Burger <joshberger6969@outlook.com>
2022-01-31 22:56:06 -0500

    Add validation for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,11 @@
 class Event < ApplicationRecord
-    
+
+    validates :name, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :location, presence: true, length: { minimum: 3, maximum: 20 }
     belongs_to :creator, class_name: 'User'
     has_many :event_attendences, foreign_key: :attended_event_id
     has_many :event_attendees, through: :event_attendences, source: :event_attendence
-    scope :past_events, -> { where('date < ?', Date.today) }
-    scope :future_events, -> { where('date >= ?', Date.today) }
+    scope :past_events, -> { where('date <= ?', Date.today).order(:date) }
+    scope :future_events, -> { where('date > ?', Date.today).order(:date) }
 
 end
