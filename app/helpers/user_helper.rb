200fc2226aa1e19c8d98087c13398b11a92ac127
Joshua Burger <joshberger6969@outlook.com>
2022-01-31 22:55:54 -0500

    Change SQL query for past events and future

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,9 @@
 module UserHelper
     def past_events
-        current_user.attended_events.where('date <= ?', Date.today)
+        current_user.attended_events.where('date < ?', Date.today).order(:date)
     end
 
     def future_events
-        current_user.attended_events.where('date > ?', Date.today)
+        current_user.attended_events.where('date > ?', Date.today).order(:date)
     end
 end
