001dd7a581366a9611425ef59fc519284973f79d
Joshua Burger <joshberger6969@outlook.com>
2022-02-03 01:07:12 -0500

    Create to_s to take price and turn to string

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,10 @@
 class Ticket < ApplicationRecord
+    
     belongs_to :event
     has_many :users, through: :event_tickets
     has_many :orders, through: :order_items
+
+    def to_s
+        price.to_s
+    end
 end
