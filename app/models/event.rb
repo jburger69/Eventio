429ca06e820b11528c835cf3a09b591dcda1cc94
Joshua Burger <joshberger6969@outlook.com>
2022-02-03 01:06:49 -0500

    has_many tickets association, accepts nested

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,13 @@ class Event < ApplicationRecord
 
     validates :name, presence: true, length: { minimum: 6, maximum: 100 }
     validates :location, presence: true, length: { minimum: 3, maximum: 20 }
+
     belongs_to :creator, class_name: 'User'
+    has_many :tickets
     has_many :event_attendences, foreign_key: :attended_event_id
     has_many :event_attendees, through: :event_attendences, source: :event_attendence
+
+    accepts_nested_attributes_for :tickets
     scope :past_events, -> { where('date <= ?', Date.today).order(:date) }
     scope :future_events, -> { where('date > ?', Date.today).order(:date) }
 
