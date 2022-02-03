d29aa65f2c0af9788e54202a1e837adbef5f9deb
Joshua Burger <joshberger6969@outlook.com>
2022-02-02 22:42:23 -0500

    Create 3 has_many associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-
+  
+  has_many :orders
+  has_many :event_tickets
+  has_many :tickets, through: :event_tickets
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event', dependent: :destroy
   has_many :event_attendences, foreign_key: :event_attendence_id
   has_many :attended_events, through: :event_attendences
